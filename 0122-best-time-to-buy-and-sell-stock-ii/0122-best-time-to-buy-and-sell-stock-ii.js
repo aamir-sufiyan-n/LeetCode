0b39776644a61d2d7caf6c76ae27cd1dd68aa9d3
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-09-23 17:05:26 +0530

    Time: 2 ms (32.72%), Space: 54.4 MB (45.24%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -5,10 +5,10 @@
 var maxProfit = function(prices) {
     
    let profit=0
-   for(let i=0;i<prices.length;i++)
+   for(let i=1;i<prices.length;i++)
    {
-    if(prices[i+1]-prices[i]>0)
-    profit+=prices[i+1]-prices[i]
+    if(prices[i-1]<prices[i])
+    profit+=prices[i]-prices[i-1]
    }
    return profit
 };
