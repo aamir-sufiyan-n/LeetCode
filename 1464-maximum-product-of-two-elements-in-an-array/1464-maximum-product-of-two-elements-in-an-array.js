56aa43a5fe756c6d2f7e53775338cc12f71dc795
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-09-27 19:04:27 +0530

    Time: 11 ms (6.41%), Space: 58.5 MB (5.13%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -3,7 +3,8 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let sorted=nums.sort()
+    let sorted=nums.sort((a,b)=>a-b)
+    console.log(sorted)
     let l=nums.length-1
     return ((sorted[l]-1)*(sorted[l-1]-1))
 };
