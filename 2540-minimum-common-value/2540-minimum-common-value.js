d8419b4fe4024ed985a612050ac3e4a5bef822a7
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-10-23 18:13:01 +0530

    Time: 1 ms (92.11%), Space: 65.3 MB (35.13%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -6,11 +6,13 @@
 var getCommon = function(nums1, nums2) {
     let n1=0
     let n2=0
+    let result=0
     while(n1<nums1.length && n2<nums2.length)
     {
-        if(nums1[n1]==nums2[n2])
-        return nums1[n1]
-        else if(nums1[n1]>nums2[n2])n2++
+        if(nums1[n1]==nums2[n2])return result=nums1[n1]
+        else if(nums1[n1]>nums2[n2]) n2++
         else n1++
     }
+    if(result==0) return -1
+    else return result
  };
