771030850c95ae084e3014eaf53dfd4c64a6efee
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-10-24 17:48:35 +0530

    Time: 30 ms (20%), Space: 59 MB (35.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,14 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let count=0
-    let splitted=s.split("")
-    for(let i=0;i<splitted.length;i++)
-    {
-        if(t.includes(splitted[i]))
-        count++
-    }
-    if(count==s.length&&count==t.length)
-    return true
-    else return false
+    return s.split("").sort().join("")===t.split("").sort().join("")
 };
