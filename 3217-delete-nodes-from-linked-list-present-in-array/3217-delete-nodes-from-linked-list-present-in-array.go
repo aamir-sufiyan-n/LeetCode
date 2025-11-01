485a98cca3d20fa705f4af969c097ab77325e862
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-11-01 12:10:45 +0530

    Time: 61 ms (32.14%), Space: 16.2 MB (37.5%) - LeetHub

---
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func modifiedList(nums []int, head *ListNode) *ListNode {
+
+    start:=&ListNode{Next:head}
+    current:=start
+
+    check:=make(map[int]bool)
+    for _,v:= range nums{
+        check[v]=true
+    }
+    for current!=nil{
+        if current.Next!=nil&&check[current.Next.Val] {
+        current.Next=current.Next.Next}else{
+        current=current.Next}
+    }
+    return start.Next
+
+}
