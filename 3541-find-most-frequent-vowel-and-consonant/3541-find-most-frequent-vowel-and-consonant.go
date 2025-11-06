40d7a0ba7a13a267ac62c8ff1292aa117bbaa6e1
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-11-06 16:22:27 +0530

    Time: 3 ms (15.31%), Space: 4.4 MB (17.5%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.go
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.go
@@ -1,10 +1,9 @@
 func maxFreqSum(s string) int {
     tracker:=make(map[rune]int)
-    for _,ch:= range s { tracker[ch]++ }
-    vowel:= map[rune]bool{
-        'a':true, 'e':true ,'i':true, 'o':true, 'u':true,
-        'A':true, 'E':true ,'I':true, 'O':true, 'U':true,
-    }
+    for _,ch:= range s { 
+        ch=unicode.ToLower(ch)
+        tracker[ch]++ }
+    vowel:= map[rune]bool{ 'a':true, 'e':true ,'i':true, 'o':true, 'u':true,  }
     vowo:=0
     leto:=0
     for ch,freq:=range tracker{
