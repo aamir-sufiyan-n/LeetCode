6298a7350f1b52ef52df653b2b5c31849778d7e3
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-11-13 14:57:47 +0530

    Time: 4 ms (64.62%), Space: 9.9 MB (68.72%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.go
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.go
@@ -1,10 +1,24 @@
 func findDuplicates(nums []int) []int {
     var res[]int
-    count:=make(map[int]int)
-    for _,num:=range nums {
-        count[num]++
-        if val,found:= count[num]; found && val==2 {res=append(res,num)}
+    // count:=make(map[int]int)
+    // for _,num:=range nums {
+    //     count[num]++
+    //     if val,found:= count[num]; found && val>1 {
+    //         res=append(res,num)
+    //         }
+    // }
+    for _,num:=range nums{
+        index:=abs(num)-1
+        if nums[index]<0{
+            res=append(res,abs(num))
+        }else{
+            nums[index]=nums[index]*(-1)
+        }
     }
-    
     return res
+}
+func abs(x int)int{
+    if x<0 {
+    return -x }
+    return x
 }
