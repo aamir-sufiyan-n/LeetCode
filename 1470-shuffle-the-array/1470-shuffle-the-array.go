f6cf1146a9e6feae82c7c533d66885f771f9458f
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-11-18 14:45:32 +0530

    Time: 5 ms (59.08%), Space: 5.4 MB (62.46%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.go
+++ 1470-shuffle-the-array/1470-shuffle-the-array.go
@@ -1,7 +1,10 @@
 func shuffle(nums []int, n int) []int {
-    var res=make([]int,0,2*n)
+    var res=make([]int,2*n)
+    j:=0
     for i:=0;i<n;i++{
-        res=append(res,nums[i],nums[i+n])
+        res[j]=nums[i]
+        res[j+1]=nums[n+i]
+        j+=2
     }
     return res
 }
