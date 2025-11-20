6116c25cb7277fc029bf3bd9debac635085caa3e
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-11-20 16:14:41 +0530

    Time: 0 ms (100%), Space: 10.1 MB (24.12%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.go
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.go
@@ -1,10 +1,11 @@
 func findMaxConsecutiveOnes(nums []int) int {
     count,res:=0,0
-    for _,n:=range nums{
-        if n==1 {
-            count++
-            if count>res { res=count }
-        }else{ count=0 } 
+    for _,n:=range nums{ 
+        if n==0{
+            count=0
+        }else{count++}
+    
+     if count>res{res=count}
     }
     return res
 }
