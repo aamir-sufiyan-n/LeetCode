0e8d5ab7d389c884509613f8cdafb827a71b4030
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2025-11-24 17:02:07 +0530

    Time: 41 ms (12.5%), Space: 4.1 MB (37.5%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.go
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.go
@@ -1,10 +1,28 @@
 func pivotInteger(n int) int {
-    sum:=(n * (n+1))/2
+    // sum:=(n * (n+1))/2
+    // for i:=1;i<=n;i++{
+    //     suml:= (i * (i+1))/2
+    //     sumr:= sum - (i*(i-1))/2
+    //     if suml == sumr{
+    //         return i
+    //     }
+    // }
+    // return -1
+
     for i:=1;i<=n;i++{
-        suml:= (i * (i+1))/2
-        sumr:= sum - (i*(i-1))/2
-        if suml == sumr{
+        sum:=0
+        sum2:=0
+        for j:=1;j<=i;j++{
+            sum+=j
+        }
+        for k:=i;k<=n;k++{
+            sum2+=k
+        }
+        if sum==sum2{
             return i
+        }else{
+            sum=0
+            sum2=0
         }
     }
     return -1
