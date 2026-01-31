ebeb88c0356030fa9796b25e00c3ccef6432d65a
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2026-01-31 16:54:09 +0530

    Time: 45 ms (18.85%), Space: 7 MB (56.77%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.go
+++ 1390-four-divisors/1390-four-divisors.go
@@ -1,21 +1,19 @@
 func sumFourDivisors(nums []int) int {
-    res:=0
-    for _,n:=range nums{
-        sum:=0
-        count:=0
-        for i:=1;i<=n;i++{
-            if n%i==0{
+    res := 0
+    for _, n := range nums {
+        sum := 0
+        count := 0
+        for i := 1; i <= int(math.Sqrt(float64(n))); i++ {
+            if n%i == 0 {
                 count++
-                sum+=i
+                sum += i
+                if i != n/i {
+                    count++
+                    sum += n / i
+                }
             }
         }
-        if count==4{
-            res+=sum
-            sum=0
-            count=0
-        }
-            sum=0
-            count=0
+        if count == 4 {res += sum}
     }
     return res
 }
