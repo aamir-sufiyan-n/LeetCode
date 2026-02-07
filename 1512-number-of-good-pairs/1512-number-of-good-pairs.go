79b1c9d8b15e6abe8a64233b9e8ec325c13d66c8
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2026-02-07 12:51:28 +0530

    Time: 0 ms (100%), Space: 4 MB (70.07%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.go
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.go
@@ -1,12 +1,20 @@
 func numIdenticalPairs(nums []int) int {
-    some:=make(map[int]int)
-    for _,num:=range nums{
-        some[num]++
-    }
+    // some:=make(map[int]int)
+    // for _,num:=range nums{
+    //     some[num]++
+    // }
     res:=0
-    for _,freq:=range some{
-        if freq>1{
-            res+=freq*(freq-1)/2
+    // for _,freq:=range some{
+    //     if freq>1{
+    //         res+=freq*(freq-1)/2
+    //     }
+    // }
+
+    for i:=0;i<len(nums);i++{
+        for j:=0;j<len(nums);j++{
+            if nums[i]==nums[j] && i<j{
+                res++
+            }
         }
     }
     return res
