a6655ae6910241e6dac94a2adaffdd910afe6575
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2026-02-09 10:04:34 +0530

    Time: 4 ms (71.96%), Space: 11.2 MB (26.25%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
@@ -8,7 +8,9 @@ func findDuplicate(nums []int) int {
 
      i:=0
      for i<len(nums){
-        if nums[i]==i{continue}
+        if nums[i]==i{
+            i++
+            continue}
         if nums[i]==nums[nums[i]]{ return nums[i]}
         nums[i],nums[nums[i]]=nums[nums[i]],nums[i]
      }
