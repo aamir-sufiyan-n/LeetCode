9a6435bbb50cb08715af192cd47885493915b282
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2026-02-09 09:56:20 +0530

    Time: 2 ms (85.71%), Space: 10.3 MB (38.75%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
@@ -1,10 +1,17 @@
 func findDuplicate(nums []int) int {
-    checker:=make(map[int]bool)
-    for _,n:=range nums{
-        if checker[n]{return n}
-        checker[n]=true
-    }
-    return -1
+    // checker:=make(map[int]bool)
+    // for _,n:=range nums{
+    //     if checker[n]{return n}
+    //     checker[n]=true
+    // }
+    // return -1
 
     //will learn floyd's later
+     i:=0
+     for i<len(nums){
+        if nums[i]==i{continue}
+        if nums[i]==nums[nums[i]]{ return nums[i]}
+        nums[i],nums[nums[i]]=nums[nums[i]],nums[i]
+     }
+     return -1
 }
