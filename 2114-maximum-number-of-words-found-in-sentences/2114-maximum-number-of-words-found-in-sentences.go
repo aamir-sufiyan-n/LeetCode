2b235e128f46b6fc452fbf474f71c8713eef2ca7
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2026-05-02 09:51:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.go
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.go
@@ -2,11 +2,11 @@ func mostWordsFound(sentences []string) int {
     res:=0
     for _,s:=range sentences{
         count:=0
-        sen:=strings.Fields(s)
-        for _,_=range sen{
+        words:=strings.Fields(s)
+        for _,_=range words{
             count++
         }
-        if count>res{res=count}
+        if count>res { res=count }
     }
     return res
 }
