ead85c70187eddf99ea4b2a82560a41c12e14fdb
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2026-05-26 10:03:16 +0530

    Time: 0 ms (100%), Space: 4.1 MB (70.59%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.go
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.go
@@ -1,14 +1,4 @@
 func numberOfSpecialChars(word string) int {
-    // upper:=make(map[rune]bool)
-    // lower:=make(map[rune]bool)
-    // for _,c:=range word{
-    //     if unicode.IsUpper(c){
-    //       upper[c]=true
-    //     }
-    //     if unicode.IsLower(c){
-    //       lower[c]=true
-    //     }
-    // }
     check:=make(map[rune]bool)
      for _,c:=range word{
           check[c]=true
