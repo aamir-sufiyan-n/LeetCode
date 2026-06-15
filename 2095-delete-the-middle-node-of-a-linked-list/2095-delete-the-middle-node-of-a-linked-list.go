76a254e973031a35bea39f9346cf6abccfc9654e
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2026-06-15 09:52:19 +0530

    Time: 0 ms (100%), Space: 11 MB (98.94%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteMiddle(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {return nil}
+    fast:=head
+    slow:=head
+    var prev *ListNode
+    for fast != nil && fast.Next != nil{
+        prev = slow
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    prev.Next = slow.Next
+    return head
+}
