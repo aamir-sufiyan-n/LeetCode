b946296b8f646b3ec767866ef26d90a3c9e900e5
Aamir Sufiyan N <aamirsufiyan848@gmail.com>
2026-09-11 10:39:05 +0530

    Time: 0 ms (100%), Space: 4.1 MB (9.88%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.go
@@ -0,0 +1,15 @@
+func countCommas(n int64) int64 {
+    var ans int64
+    for _,x:= range []int64{
+        999,
+        999999,
+        999999999,
+        999999999999,
+        999999999999999,
+    }{
+        if n>x{
+            ans+=n-x
+        }
+    }
+    return ans
+}
